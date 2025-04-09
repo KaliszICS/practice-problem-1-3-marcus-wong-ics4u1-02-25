a5d114edfd33eedc406373d4bbcd71195d054893
marcus-wong-ics4u1-02-25 <marcus.wong5@student.tdsb.on.ca>
2025-04-09 06:36:42 +0000

    same assignment 1.3

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,59 @@
+import java.io.*;
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
+	}
+	public static String readFile(String filename) {
+		FileReader inputStream = null;
+		String result = "";
+		
+		try {
+			inputStream = new FileReader(filename);
+			int c;
+			while ((c = inputStream.read()) != -1) {
+				result += (char)c;
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+		return result;
+	}
+	public static String backwardsReadFile(String filename) {
+		FileReader inputStream = null;
+		String result = "";
+		
+		try {
+			inputStream = new FileReader(filename);
+			String temp = "";
+			int c;
+			while ((c = inputStream.read()) != -1) {
+				temp += (char)c;
+			}
+			for (int i = temp.length() - 1; i >= 0; i--) {
+				result += temp.charAt(i);
+			}
+		} catch (IOException e) {
+			System.out.println(e);
+		} finally {
+			try {
+				if (inputStream != null) {
+					inputStream.close();
+				}
+			} catch (IOException e) {
+				System.out.println(e);
+			}
+		}
+		return result;
 	}
 
 	
